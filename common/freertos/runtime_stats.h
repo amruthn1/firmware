1a09b4b32c38a8d1badc6d358043a749abfecba4
amruthn1 <amruthnadimpally1@gmail.com>
2026-01-28 23:31:25 -0500

    use dwt clock instead of tim2

--- common/freertos/runtime_stats.h
+++ common/freertos/runtime_stats.h
@@ -13,16 +13,6 @@
     #error "MCU Arch not supported for runtime stats"
 #endif
 
-#if defined(STM32F407xx) || defined(STM32F732xx)
-    #define RUNTIME_TIM       TIM2
-    #define RUNTIME_TIM_CLK() (RCC->APB1ENR |= RCC_APB1ENR_TIM2EN)
-#elif defined(STM32G474xx)
-    #define RUNTIME_TIM       TIM2
-    #define RUNTIME_TIM_CLK() (RCC->APB1ENR1 |= RCC_APB1ENR1_TIM2EN)
-#else
-    #error "MCU Arch not supported for runtime stats"
-#endif
-
 void configureTimer(void);
 uint32_t getCounterValue(void);
 
